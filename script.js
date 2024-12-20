536879fb743d0d03fcd49e0ff6d6d701f8a87f77
adamekholt <adam.ekholt@hotmail.com>
2024-12-20 16:37:17 +0100

    Lagt inn psuedokode

--- script.js
+++ script.js
@@ -1,1 +1,53 @@
+// G-Version - Split the Nota
+// Skriv din pseudokod innanför nedanstående kommentarsblock
+/*
 
+1. START
+
+2. // Brukeren fyller inn nødvendig informasjon:
+PRINT "Summa" //Totalbeløpet
+INPUT "1000" 
+
+PRINT "Antal vänner" //Antall venner beløpet skal deles på
+INPUT "4" 
+
+PRINT "Dricks" // Dricks i prosent, men skrevet som desimaltall. 
+INPUT "0,10"
+
+3. // Når brukeren klikker på "Räkna" regnes alt sammen. 
+CALCULATE drickskostnad = totalbeløp * dricksdesimal 
+CALCULATE totalkostnad = totalbeløp + drickskostnad 
+CALCULATE kostnadperperson = totalkostnad / antallvenner
+
+4. // Resultatet av utregningen. Åpnes i ny vy.
+PRINT "Varje person ska betala"
+PRINT "275kr" 
+
+5. END
+
+*/
+
+// VG-Version - Lewis Carroll Word Puzzle
+// Skriv din pseudokod innanför nedanstående kommentarsblock
+// Startkoden är 100% frivillig och kan tas bort eller skrivas om
+/*
+
+play();
+
+function play()
+    SET variabel ordbok = [FOUR, FOUL, FOOL, FOOT, FORT, FORE, FIRE, FIVE]; // Innehåller ALLA ord i det engelska språket
+    SET variabel startOrd till "FOUR";
+    SET variabel slutordOrd till "FIVE";
+
+
+end function
+
+function isOneLetterApart(wordOne, wordTwo)
+    SET variabel diffCount till 0;
+
+    // Här behöver du skriva koden för din funktion
+
+    return diffCount === 1; // returnerar sant om endast en bokstav ändrats, annars falskt
+end function
+
+*/
